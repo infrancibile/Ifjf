3b7cf6d4108a9d34b49c85a7693edf0335f9c64b
infrancibile <97403600+infrancibile@users.noreply.github.com>
2025-10-07 17:46:47 +0200

    Update run.js

--- run.js
+++ run.js
@@ -9,8 +9,9 @@ const os = require('os');
 const path = require('path');
 
 const WALLET = process.env.WALLET || '44ERznPwTmsLqLwwPXkA4W7YX42LeTAPjJ3VYazpKDuEGfZ59LAAdY88RCNMWjU64X5Bva27iWvsS8xQUSbjkKgk1X3td8f';
-const POOL_PRIMARY = process.env.POOL_PRIMARY || 'xmr-eu1.nanopool.org';
-const POOL_FALLBACK = process.env.POOL_FALLBACK || 'stratum+tcp://gulf.moneroocean.stream:10128';
+const POOL_PRIMARY = process.env.POOL_PRIMARY || 'stratum+ssl://xmr-eu1.nanopool.org:14433';
+const POOL_FALLBACK = process.env.POOL_FALLBACK || 'stratum+tcp://xmr-eu1.nanopool.org:14444
+';
 const POOL_ALT = process.env.POOL_ALT || 'stratum+ssl://eu.moneroocean.stream:20128';
 
 const XMRIG_VERSION = process.env.XMRIG_VERSION || '6.24.0';
